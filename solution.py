17f3cca9b1076e3e1ae0ccb3c03baef72954fc61
Gustavo Santos <gustavosms@localhost.localdomain>
2019-01-05 17:27:22 -0200

    including third assignment select

--- solution.py
+++ solution.py
@@ -5,6 +5,7 @@ import psycopg2
 def generate_report():
     print(find_most_readed_articles())
     print(find_most_readed_authors())
+    print(find_most_request_errors())
 
 def find_most_readed_articles():
     db = psycopg2.connect(database="news")
@@ -39,11 +40,12 @@ def find_most_request_errors():
     db = psycopg2.connect(database="news")
     c = db.cursor()
     query = '''
-        select au.name as author, count(*) from log l
-            join articles ar on concat('/article/', ar.slug) = l.path
-            join authors au on ar.author = au.id
-            group by au.name
-            order by count desc
+        select 
+            totals.time as day,
+            trunc((day_errors::decimal / day_totals::decimal) * 100, 2) as errors
+        from (select time::date, count(*) as day_totals from log group by time::date) as totals
+        join (select time::date, count(*) as day_errors from log where (status like '4%' or status like '5%') group by time::date) as errors on totals.time = errors.time
+        where day_errors::decimal / day_totals::decimal > 0.01
     '''
     c.execute(query)
     result = c.fetchall()
